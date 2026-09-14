ef89a8995ba7bb46e8bb461637973051ed4bf080
Mika-shi <ayperiaidarova.com>
2026-09-14 22:55:36 +0600

    Added follow logics

--- HW_58/HW_58/Controllers/ProfileController.cs
+++ HW_58/HW_58/Controllers/ProfileController.cs
@@ -32,7 +32,7 @@ public class ProfileController : Controller
         return RedirectToAction("Details", new { id = currentUserId });
     }
 
-    public IActionResult Details(string id)
+    public async  Task<IActionResult> Details(string id)
     {
         User? user = _context.Users
             .Include(user => user.Posts)
@@ -46,7 +46,124 @@ public class ProfileController : Controller
         string? currentUserId = _userManager.GetUserId(User);
 
         ViewBag.IsMyProfile = currentUserId == user.Id;
+        
+        bool isFollowing = false;
+
+        if (currentUserId != null && currentUserId != user.Id)
+        {
+            isFollowing = await _context.Follows
+                .AnyAsync(follow =>
+                    follow.FollowerId == currentUserId &&
+                    follow.FollowingId == user.Id);
+        }
+
+        ViewBag.IsFollowing = isFollowing;
 
         return View(user);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize]
+    public async Task<IActionResult> Follow(string id)
+    {
+        string? currentUserId = _userManager.GetUserId(User);
+
+        if (currentUserId == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        if (currentUserId == id)
+        {
+            return RedirectToAction("Details", new { id = id });
+        }
+
+        User? currentUser = await _userManager.FindByIdAsync(currentUserId);
+        User? targetUser = await _userManager.FindByIdAsync(id);
+
+        if (currentUser == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        if (targetUser == null)
+        {
+            return NotFound();
+        }
+
+        bool alreadyFollowing = await _context.Follows
+            .AnyAsync(follow =>
+                follow.FollowerId == currentUserId &&
+                follow.FollowingId == id);
+
+        if (!alreadyFollowing)
+        {
+            Follow follow = new Follow
+            {
+                FollowerId = currentUserId,
+                FollowingId = id,
+                CreatedOn = DateTime.UtcNow
+            };
+
+            _context.Follows.Add(follow);
+
+            currentUser.FollowingCount++;
+            targetUser.FollowersCount++;
+
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToAction("Details", new { id = id });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize]
+    public async Task<IActionResult> Unfollow(string id)
+    {
+        string? currentUserId = _userManager.GetUserId(User);
+
+        if (currentUserId == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        User? currentUser = await _userManager.FindByIdAsync(currentUserId);
+        User? targetUser = await _userManager.FindByIdAsync(id);
+
+        if (currentUser == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        if (targetUser == null)
+        {
+            return NotFound();
+        }
+
+        Follow? follow = await _context.Follows
+            .FirstOrDefaultAsync(follow =>
+                follow.FollowerId == currentUserId &&
+                follow.FollowingId == id);
+
+        if (follow != null)
+        {
+            _context.Follows.Remove(follow);
+
+            if (currentUser.FollowingCount > 0)
+            {
+                currentUser.FollowingCount--;
+            }
+
+            if (targetUser.FollowersCount > 0)
+            {
+                targetUser.FollowersCount--;
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToAction("Details", new { id = id });
     }
 }
